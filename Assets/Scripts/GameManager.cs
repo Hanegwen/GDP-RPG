6e6f67e4c461946c861c1bb6a546e5eaa8862696
Mike Morrone <Hanegwen725@gmail.com>
2018-02-15 15:15:43 -0600

    Class Work

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //[SerializeField]
+    //Player[] players;
+    public int currentPlayer;
+
+    Canvas playerCanvas;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
