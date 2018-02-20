04efc2eec8d22b858ce77022373b597b38bfac87
Mike Morrone <Hanegwen725@gmail.com>
2018-02-20 13:48:04 -0600

    Fixed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,21 +4,97 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    //[SerializeField]
-    //Player[] players;
-    public int currentPlayer;
 
-    Canvas playerCanvas;
+    [SerializeField]
+    int attackNum = 0; //base 0
+
+    [SerializeField]
+    int playerCount; //base 1
+    [SerializeField]
+    Players[] players; //Current Player Count make 2 in Engine
+
+
+    public int currentPlayer; //base 1
+
+    [SerializeField]
+    bool attack = false;
+
+    float attackDamage;
 
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
+        playerCount = players.Length;
+
+        for (int i = 0; i < playerCount; i++)
+        {
+            players[i] = Instantiate(players[1]);
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        attack = players[currentPlayer - 1].canAttack;
+
+        CombatSystem();
+        CheckDeath();
+        
 	}
+
+    void CombatSystem()
+    {
+        players[currentPlayer - 1].myCanvas.enabled = true;
+        attackNum = players[currentPlayer - 1].ReturnAttack();
+
+        if (attack)
+        {
+            attackDamage = players[currentPlayer - 1].Attack(attackNum);
+            if(currentPlayer + 1 > playerCount)
+            {
+                players[0].TakeDamage(attackDamage);
+            }
+
+            else
+            {
+                players[currentPlayer].TakeDamage(attackDamage);
+            }
+
+            players[currentPlayer - 1].canAttack = false;
+
+            NextPlayer();
+        }
+
+    }
+
+    void NextPlayer()
+    {
+        players[currentPlayer - 1].myCanvas.enabled = false;
+        currentPlayer++;
+
+        if (currentPlayer > playerCount)
+        {
+            currentPlayer = 1;
+        }
+        players[currentPlayer - 1].myCanvas.enabled = true;
+    }
+
+    void CheckDeath()
+    {
+        foreach (Players player in players)
+        {
+            if(player.Dead)
+            {
+                GameOver();
+            }
+        }
+    }
+
+    void GameOver()
+    {
+        //call GameOver Canvas
+    }
+
+    
 }
---
+++ Assets/Scripts/Players.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players : MonoBehaviour {
+
+    public enum attacks {test1, test2, test3, test4 };
+
+    public attacks currentAttack;
+
+    public bool canAttack = false;
+
+    int attackNum;
+
+    bool dead = false;
+
+    int damage = 0;
+
+    public Canvas myCanvas;
+
+    public bool Dead
+    {
+        get
+        {
+            return dead;
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        myCanvas.enabled = false;
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    public void TakeDamage(float damage)
+    {
+
+    }
+
+    public float Attack(int attackValue)
+    {
+
+        return damage;
+    }
+
+    public int ReturnAttack()
+    {
+        return attackNum;
+    }
+
+    public void OnAttack1Click()
+    {
+        attackNum = 0;
+        canAttack = true;
+    }
+
+    public void OnAttack2Click()
+    {
+        attackNum = 1;
+        canAttack = true;
+    }
+
+    public void OnAttack3Click()
+    {
+        attackNum = 2;
+        canAttack = true;
+    }
+
+    public void OnAttack4Click()
+    {
+        attackNum = 3;
+        canAttack = true;
+    }
+}
