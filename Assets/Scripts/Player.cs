8912ff86a681deab7f4d2c4d311c3e4a8b834755
CamHargrave <tvastacarious@gmail.com>
2018-02-22 13:34:26 -0600

    Player script
    I added the spot for the stats. I think this is right lmk if not and i need to change something.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField]
+    public int Health;
+    [SerializeField]
+    public int Damage;
+    [SerializeField]
+    public int Armor;
+    [SerializeField]
+    public int Healing;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
