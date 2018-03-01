37cf01572a56e8174c018467a3a1f447004fa5bb
jackmalboeuf <Jack.Malboeuf@iam.local>
2018-03-01 15:17:30 -0600

    main menu

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    [SerializeField]
+    string nextScene;   //name of character select screen
+    [SerializeField]
+    GameObject creditsPanel;    //image containing credits
+    [SerializeField]
+    GameObject quitPopUpImage;  //image asking if player is sure they want to quit
+
+    public void StartButtonPressed()
+    {
+        SceneManager.LoadScene(nextScene);  //load character select scene
+    }
+
+    public void CreditsButtonPressed()  //credits button and credits back button
+    {
+        if (creditsPanel.activeSelf)
+            creditsPanel.SetActive(false);
+        else
+            creditsPanel.SetActive(true);
+    }
+
+    public void QuitButtonPressed() //quit button and quit back button
+    {
+        if (quitPopUpImage.activeSelf)
+            quitPopUpImage.SetActive(false);
+        else
+            quitPopUpImage.SetActive(true);
+    }
+
+    public void QuitGameButtonPressed() //actual quit button
+    {
+        Application.Quit(); //closes down application
+    }
+}
