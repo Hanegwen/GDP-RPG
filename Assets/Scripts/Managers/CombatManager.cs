cbe16b388a14e6efb055353756194c1a6c06874d
Mike Morrone <Hanegwen725@gmail.com>
2018-03-09 10:09:25 -0600

    Refactored and Working on Images

---
+++ Assets/Scripts/Managers/CombatManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    UIManager uiManager;
+
+    [SerializeField]
+    public bool attack = false;
+
+    float attackDamage;
+
+    [SerializeField]
+    int attackNum = 0; //base 0
+
+    GameObject activePlayer;
+
+    public int currentPlayer; //base 1
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        uiManager = GetComponent<UIManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void MoveImage()
+    {
+        uiManager.playerImages[currentPlayer - 1].gameObject.transform.position = new Vector2(0, 0);
+        uiManager.playerImages[currentPlayer - 1].gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+        if (currentPlayer == uiManager.playerCount)
+        {
+            Debug.Log("Calling 0");
+            uiManager.playerImages[0].gameObject.transform.position = new Vector2(2, 0);
+            uiManager.playerImages[0].gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            Debug.Log("Calling The Position");
+            uiManager.playerImages[currentPlayer].gameObject.transform.position = new Vector2(2, 0);
+            uiManager.playerImages[currentPlayer].gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+
+    public void CombatSystem() //How the Players attack
+    {
+        uiManager.players[currentPlayer - 1].myCanvas.enabled = true; //Current Players Menu System
+        uiManager.playerImages[currentPlayer - 1].SetActive(true); //Current Players Avatar
+
+        MoveImage();
+
+        activePlayer = uiManager.playerImages[currentPlayer - 1];
+        attackNum = uiManager.players[currentPlayer - 1].ReturnAttack(); //Keeps track of attack numbers
+
+        if (attack) //When Player clicks a button the attack goes through
+        {
+            attackDamage = uiManager.players[currentPlayer - 1].Attack(attackNum); //The attack amount
+            attackDamage = 10; //Shows Combat Example working
+            Debug.Log(attackDamage);
+            if (currentPlayer + 1 > uiManager.playerCount) //Checks if next player is past array and then sets him to 0 in case
+            {
+                uiManager.players[0].TakeDamage(attackDamage);
+            }
+
+            else //Deals damage to the next player
+            {
+                uiManager.players[currentPlayer].TakeDamage(attackDamage);
+            }
+
+            uiManager.players[currentPlayer - 1].canAttack = false; // Stops attacking
+
+            NextPlayer(); //Calls the next player
+        }
+
+    }
+
+    void NextPlayer() //When a Player attacks the next player in the list becomes the active player
+    {
+        uiManager.players[currentPlayer - 1].myCanvas.enabled = false;
+        //playerImages[currentPlayer - 1].SetActive(false);
+        currentPlayer++;
+
+        if (currentPlayer > uiManager.playerCount)
+        {
+            currentPlayer = 1;
+        }
+        uiManager.players[currentPlayer - 1].myCanvas.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject[] playerImages = new GameObject[2];
+    [SerializeField]
+    public int playerCount; //base 1
+
+    [SerializeField]
+    public Player[] players; //Current Player Count make 2 in Engine
+
+    bool choosingState = true;
+
+    [SerializeField]
+    GameObject[] imageOptions;
+
+    int currentImage = 0;
+
+    int playerChoosing = 0;
+
+    [SerializeField]
+    Transform imageLocation;
+
+    [SerializeField]
+    Canvas DeathCanvas;
+
+    [SerializeField]
+    Canvas playerSelectCanvas;
+
+    [SerializeField]
+    Text playerNumber;
+
+    CombatManager combatManager;
+
+    // Use this for initialization
+    void Start ()
+    {
+        combatManager = GetComponent<CombatManager>();
+        playerCount = players.Length;
+
+        foreach (GameObject o in imageOptions)
+        {
+            Instantiate(o, imageLocation.transform.position,Quaternion.identity, null);
+            o.transform.SetAsFirstSibling();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (choosingState)
+        {
+            ChoosingUI();
+        }
+
+        if (!choosingState)
+        {
+            combatManager.attack = players[combatManager.currentPlayer - 1].canAttack;
+            combatManager.CombatSystem();
+            CheckDeath();
+        }
+
+        
+
+    }
+
+    void ChoosingUI()
+    {
+        playerNumber.text = "Player" + (playerChoosing + 1);
+    }
+
+    public void PickImage()
+    {
+        playerImages[playerChoosing] = imageOptions[currentImage];
+
+        playerChoosing++;
+
+        if (playerChoosing > 1)
+        {
+            for (int i = 0; i < playerCount; i++) //On Start the Players are created
+            {
+                players[i] = Instantiate(players[i]);
+                players[i].myPlayerNumber = i + 1;
+                playerImages[i] = Instantiate(playerImages[i]);
+                //playerImages[i].SetActive(false);
+
+            }
+            playerSelectCanvas.enabled = false;
+            choosingState = false;
+        }
+
+    }
+
+    public void NextButton()
+    {
+        imageOptions[currentImage].SetActive(false);
+        currentImage++;
+        if (currentImage > playerImages.Length)
+        {
+            currentImage = 0;
+        }
+        imageOptions[currentImage].SetActive(true);
+
+    }
+
+    void GameOver(string playerName) //Calls the end screen
+    {
+        DeathCanvas.transform.GetComponentInChildren<Text>().text = playerName + " has died";
+        DeathCanvas.enabled = true;
+    }
+
+    void CheckDeath() //Check if a player is dead, if so other person wins
+    {
+        foreach (Player player in players)
+        {
+            if (player.Dead)
+            {
+                GameOver(player.name);
+            }
+        }
+    }
+}
